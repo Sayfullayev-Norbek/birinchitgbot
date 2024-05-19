27eb990b47684192797378c702dee4f36ba960f2
Sayfullayev Norbek <sayfullayev1572norbek@gmail.com>
2024-05-19 23:11:31 +0500

    Tgbot yozgan odamni yozganini o'ziga qaytarishni qildik

--- tgbot.php
+++ tgbot.php
@@ -1,7 +1,11 @@
 <?php
 
-echo "Tg bot";
+include 'Telegram.php';
 
+$telegram = new Telegram("6387497149:AAHBl_3ZZvF00q8zNgDCuQFQ0YvXGCirync");
+$text = $telegram->Text();
+$chat_id = $telegram->ChatID();
+$content = array('chat_id' => $chat_id, 'text' => $text);
+$telegram->sendMessage($content);
 
-//telegram bot
 ?>
