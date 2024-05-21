81ebb8cb553430dd65edbe19429accb7a246d7c0
Sayfullayev Norbek <sayfullayev1572norbek@gmail.com>
2024-05-21 14:14:31 +0500

    Telegram start bosganda uzingning id va boshqa ma'lumotlarini olib ko'rsatadi

--- telegramtoken.php
+++ telegramtoken.php
@@ -1,2 +1,3 @@
 <?php
 $telegram = new Telegram("6387497149:AAHBl_3ZZvF00q8zNgDCuQFQ0YvXGCirync");
+
--- tgbot.php
+++ tgbot.php
@@ -1,23 +1,26 @@
 <?php
 
 include 'Telegram.php';
-require_once "telegramtoken.php";
+
+include 'telegramtoken.php';
 
 $chat_id = $telegram->ChatID();
 $text = $telegram->Text();
 $first_name = $telegram->FirstName();
 $last_name = $telegram->LastName();
+$username = $telegram->Username();
+$message_id = $telegram->MessageID();
 if($text == "/start"){
     $option = array(
 
         array($telegram->buildKeyboardButton("✉️ Batavsil ma'lumot")),
 
-        array($telegram->buildKeyboardButton("🖼 Rasm"), $telegram->buildKeyboardButton("Musiqa")),
+        array($telegram->buildKeyboardButton("🖼 Rasm"), $telegram->buildKeyboardButton("ID")),
 
         array($telegram->buildKeyboardButton("Video"), $telegram->buildKeyboardButton("👨🏻‍💻 Tuzuvchi")),
     );
     $keyb = $telegram->buildKeyBoard($option, $onetime=true, $resize = true);
-    $content = array('chat_id' => $chat_id, 'text' => "Salom, $first_name $last_name , botimizga xush kelibsiz!");
+    $content = array('chat_id' => $chat_id, 'text' => "ID -> $chat_id  name -> $last_name", 'parse_mode' => "HTML");
     $telegram->sendMessage($content);
     $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Bizdan sizga qanday xizmat kerak!");
     $telegram->sendMessage($content);
@@ -26,6 +29,14 @@ if($text == "/start"){
     $telegram->sendMessage($content);
 }elseif ($text == "✉️ Batavsil ma'lumot"){
     $content = array('chat_id' => $chat_id, 'text' => "Kuzatuv ma'lumotlari bo'lmagan hollarda yillik oqim me'yorini aniqlash quyida keltirilgan usullardan birini qabul qilish mumkin.");
+    $telegram->sendMessage($content);
+}elseif ($text == "ID"){
+    $content = array('chat_id' => $chat_id, 'text' => " 👤 You
+ ├ id: $chat_id
+ ├ first_name: $first_name
+ ├ last_name: $last_name
+ ├ username: <a href='https://t.me/$username'>$username </a>
+ ├ language_code: uz (-) ", 'parse_mode' => "HTML");
     $telegram->sendMessage($content);
 }
 ?>
